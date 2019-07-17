0c1977a52a3b5c8bd627abbf9d6031417d939358
kanw <kanw@cisco.com>
2019-07-17 15:23:25 +0800

    rename main.py

--- main_server.py
+++ main_server.py
@@ -1,18 +1,22 @@
 import sys
 from tornado import httpserver, ioloop, web, options
-from asynchronization import async_function
+from src.asynchronization import async_function
 
-class MainHandler(tornado.web.RequestHandler):
+class MainHandler(web.RequestHandler):
     def get(self):
         self.write("Hello, world! My name is Eagle.")
 
 if __name__ == "__main__":
 	async_function.test_func()
+
+	settings = dict(
+		autoreload=True,
+	)
 	handlers = [
 		(r"/", MainHandler),
 	]
 
-	app = tornado.web.Application(handlers)
+	app = web.Application(handlers, settings)
 	http_server = httpserver.HTTPServer(app)
-    http_server.listen(9012)
-    ioloop.IOLoop.current().start()
+	http_server.listen(9012)
+	ioloop.IOLoop.current().start()
